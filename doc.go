b65d20b5d130215d1db89f472fdcc7546898c238
flowerinthenight <chew.esmero@mobingi.com>
2019-01-17 15:37:37 +0900

    add main doc

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package sdk-go is Mobingi's main SDK for the Go (golang) programming language.
+//
+// Please check out our documentation at https://docs.mobingi.com/.
+//
+// GitHub repository: https://github.com/mobingi/sdk-go/
+package sdk-go
