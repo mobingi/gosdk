2d6a71f42db4c7ad48104182cd14dfcc0f94df60
flowerinthenight <chew.esmero@mobingi.com>
2019-01-17 15:35:06 +0900

    refactor

--- pkg/util/simplelog/simplelog.go
+++ pkg/util/simplelog/simplelog.go
@@ -15,22 +15,6 @@ func Info(v ...interface{}) {
 	log.Printf("%s %s", green("[info]"), m)
 }
 
-// Error prints `v` into standard output (via log) with a red prefix "error:".
-func Error(v ...interface{}) {
-	red := color.New(color.FgRed).SprintFunc()
-	m := fmt.Sprintln(v...)
-	log.Printf("%s %s", red("[error]"), m)
-}
-
-// ErrorD prints `v` into standard output (via log) with a red prefix "error:" along with stack trace.
-func ErrorD(v ...interface{}) {
-	red := color.New(color.FgRed).SprintFunc()
-	m := fmt.Sprintln(v...)
-	log.Printf("%s %s", red("[error]"), m)
-	err := errors.WithStack(fmt.Errorf(m))
-	fmt.Printf("%+v\n", err)
-}
-
 // Infof is the formatted version of Info().
 func Infof(format string, v ...interface{}) {
 	green := color.New(color.FgGreen).SprintFunc()
@@ -38,9 +22,25 @@ func Infof(format string, v ...interface{}) {
 	log.Printf("%s %s", green("[info]"), m)
 }
 
+// Error prints `v` into standard output (via log) with a red prefix "error:".
+func Error(v ...interface{}) {
+	red := color.New(color.FgRed).SprintFunc()
+	m := fmt.Sprintln(v...)
+	log.Printf("%s %s", red("[error]"), m)
+}
+
 // Errorf is the formatted version of Error().
 func Errorf(format string, v ...interface{}) {
 	red := color.New(color.FgRed).SprintFunc()
 	m := fmt.Sprintf(format, v...)
 	log.Printf("%s %s", red("[error]"), m)
+}
+
+// ErrorD prints `v` into standard output (via log) with a red prefix "error:" along with a stack trace.
+func ErrorD(v ...interface{}) {
+	red := color.New(color.FgRed).SprintFunc()
+	m := fmt.Sprintln(v...)
+	log.Printf("%s %s", red("[error]"), m)
+	err := errors.WithStack(fmt.Errorf(m))
+	fmt.Printf("%+v\n", err)
 }
