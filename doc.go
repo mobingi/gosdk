a67147dc03e31fa0103923bcc742dddfaaba6e6c
flowerinthenight <chew.esmero@gmail.com>
2019-02-21 11:40:23 +0900

    try remove base doc

--- doc.go
+++
@@ -1,6 +0,0 @@
-// Package sdk is Mobingi's main SDK for the Go (golang) programming language.
-//
-// Please check out our documentation at https://docs.mobingi.com/.
-//
-// GitHub repository: https://github.com/mobingi/sdk-go/
-package sdk
