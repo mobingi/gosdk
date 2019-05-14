9ea57e0302a7151a2020b1acb77ebd7d38ca178a
flowerinthenight <chew.esmero@mobingi.com>
2019-05-14 15:03:12 +0900

    add namespace pkg

---
+++ mobingi/namespace/namespace.go
@@ -0,0 +1,11 @@
+package namespace
+
+const (
+	Dashboard = "dashboard"
+	User      = "user"
+	Rbac      = "rbac"
+	Ripple    = "ripple"
+	Wave      = "wave"
+	Ocean     = "ocean"
+	Magnet    = "magnet"
+)
